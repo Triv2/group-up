f47956dbbc1da455b29a598ef15cdf7b6b338c7e
Trivial-lore <triviallore@gmail.com>
2023-11-01 21:09:38 -0500

    started landing page hero

--- app/(front)/landing/page.tsx
+++ app/(front)/landing/page.tsx
@@ -4,7 +4,15 @@ interface LandingPageProps {}
 const LandingPage = () => {
   return (
 <div>
-LandingPage
+
+<div className="flex flex-col items-center justify-center">
+<h1>HELLO GROUPIES!</h1>
+<h2>Welcome to the internets hottest new grouping application.</h2>
+<h3>Align with groups that have your interests quickly.</h3>
+<h4>Plan events with your friends in a public or private setting.</h4>
+<h5>and much... much... more.</h5>
+</div>
+
 </div>
   );
 }
