931f16216b45eb79fddcc267f1e75725faac186c
Trivial-lore <triviallore@gmail.com>
2023-11-03 21:58:29 -0500

    add tabs to thread viewer

---
+++ components/thread/thread-viewer.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { Tab, Tabs } from '@nextui-org/react';
+import {useState, useEffect} from'react'
+
+interface ThreadViewerProps {}
+
+const ThreadViewer:React.FC<ThreadViewerProps> = () => {
+
+const [isMounted, setIsMounted] = useState(false);
+
+useEffect(() => {
+setIsMounted(true);
+}, []);
+
+if (!isMounted) {
+return null;
+}
+  return (
+    <div>
+      <Tabs>
+        Categories:
+        <Tab key="announcements" title="Announcements">
+          Announcements
+        </Tab>
+        <Tab key="events" title="Events">
+          Events
+        </Tab>
+        <Tab key="tasks" title="Tasks">
+          Tasks
+        </Tab>
+        <Tab key="groupChat" title="Group Chat">
+          Group Chat
+        </Tab>
+      </Tabs>
+    </div>
+  );
+}
+export default ThreadViewer;
