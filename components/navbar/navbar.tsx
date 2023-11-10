2b4d11d9611148563da3899786870ce4d3dff9bb
Trivial-lore <triviallore@gmail.com>
2023-11-09 20:00:02 -0600

    navbar styling

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface NavbarProps {}
 
 const Navbar = () => {
   return (
-<nav className="sticky w-full h-50 flex items-center justify-between px-10 bg-zinc-200/80">
-    <div>
-      <Image src="/images/logo.png" width={25} height={25} alt="logo" />
-      <p>Logo Text</p>
+<nav className="sticky w-full h-[35px] flex items-center justify-between px-10 bg-zinc-200/90 shadow-xl">
+    <div className="flex items-center justify-center gap-1">
+      <Image src="/logo.png" width={25} height={25} alt="logo" />
+      <p>Group Up</p>
     </div>
     <div>
-      NavButtons
+      <Link href="/sign-in">
+        Sign In
+      </Link>
+      <Link href="/sign-up">
+        Sign up
+      </Link>
     </div>
 </nav>
   );
