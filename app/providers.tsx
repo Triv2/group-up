0dff5ffce6b15fdc15addbae88efab20018891c9
Trivial-lore <triviallore@gmail.com>
2023-11-10 13:47:41 -0600

    added dark mode, setting up the styling now

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,20 @@
 // app/providers.tsx
 'use client'
 
+import { ThemeProvider } from '@/components/providers/theme-provider'
 import {NextUIProvider} from '@nextui-org/react'
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
       {children}
+      </ThemeProvider>
     </NextUIProvider>
   )
 }
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { UserButton, currentUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { ModeToggle } from "./mode-toggle";
 
 interface NavbarProps {}
 
@@ -12,7 +13,7 @@ const Navbar = async () => {
   const user = await currentUser() || null;
   const name= `${user?.firstName} ${user?.lastName}`;
   return (
-<nav className="fixed w-full h-[35px] flex items-center justify-between px-2  bg-zinc-200 shadow-xl">
+<nav className="fixed w-full h-[35px] flex items-center justify-between px-2 z-50 bg-zinc-200 dark:bg-zinc-700 shadow-xl">
    
     {!user ? (
       <Link className="flex items-center justify-center gap-1" href="/">  
@@ -36,23 +37,27 @@ const Navbar = async () => {
      Sign up
    </Link>
    <UserButton afterSignOutUrl="/"/>
+   <ModeToggle/>
  </div>) 
         : (
           <div>
             <div className="flex justify-between items-center w-full gap-1 p-2">
               
-            <h1 className="text-xs md:text-md font-bold">Welcome</h1> 
+            <h1 className="text-xs md:text-md font-bold hidden sm:block">Welcome</h1> 
             <h1 className="text-xs md:text-md font-bold  bg-gradient-to-tr from-green-500 to-green-700 bg-clip-text text-transparent"> {name}!</h1>
             
-
+          
           <UserButton afterSignOutUrl="/" 
             appearance={{
               elements: {
                 userButtonPopoverCard:"pointer-events-auto",
+                userButtonAvatarBox: "h-4 w-5",
               }
             }}
           
           />
+          
+          <ModeToggle/>
           </div>
           
 
